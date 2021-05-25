857e0cf9ec61bb5e35dca75763811229774ea07d
Viktor_Bor <jazzyvibe1325@gmail.com>
2021-05-25 23:23:28 +0200

    first step

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {recepts} from './store.js'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>hello react</h1>
+      <h1>Favorite Recepts: </h1>
+      <hr></hr>
+      <h1>All Recepts:</h1>
+      <div id='All-Recepts'>
+        {recepts.map(recept => (
+        <div className="card" style={{width: '30rem'}}>
+                    <img src={recept.img} alt='' className="card-img-top"/>
+        <div className="card-body">
+          <h5 className="card-title">{recept.name}</h5>
+          <a id="btn" href="#" className="btn btn-primary">Add favorite</a>
+          <a id="btn"href="#" className="btn btn-danger">About</a>
+        </div>
+        </div>
+       ))
+    }
+</div>
     </div>
   ); 
 }
